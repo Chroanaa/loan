b67df1df6ed0ec33f9145be4e83c7e595145f8ec
Christian Alfonso <alfonso.christian.macatangay@gmail.com>
2024-12-22 14:52:56 +0800

    revised login from officers, forgot pass, change pass

--- view/loginOfficers.php
+++ view/loginOfficers.php
@@ -11,7 +11,12 @@ $errors = include '../controller/loginOfficersController.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container mt-5">
+
+<div class="officer-page d-flex justify-content-center align-items-center" 
+     style="min-height: 100vh; background: url(../wwwroot/img/company-bg.jpg)no-repeat center / cover;">
+
+    <div class="container p-4 rounded-3 text-light row">
+        <div class="col-sm-12 col-md-6 ">
         <h2>Officers Login</h2>
         <?php
         if (!empty($errors)) {
@@ -35,7 +40,14 @@ $errors = include '../controller/loginOfficersController.php';
             </div>
             <button type="submit" name="login" class="btn btn-primary">Login</button>
         </form>
+        </div>
+
+        <div class="col-sm-12 col-md-6 mt-3">
+            <img src="../wwwroot/img/hero-officers-bg.jpg" class="img-fluid" alt="">
+        </div>
     </div>
+    
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
--- view/resetPassword.php
+++ view/resetPassword.php
@@ -11,7 +11,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container mt-5">
+<div class="client-page d-flex justify-content-center align-items-center"
+style="background:url(../wwwroot/img/hero-bg.jpg) no-repeat center /cover; min-height: 100vh;">
+    <div class="container bg-light rounded-3 p-4">
         <h2>Reset Password</h2>
         <?php
         if (!empty($errors)) {
@@ -35,6 +37,7 @@
             <button type="submit" class="btn btn-primary">Reset Password</button>
         </form>
     </div>
+</div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="js/script.js"></script>
